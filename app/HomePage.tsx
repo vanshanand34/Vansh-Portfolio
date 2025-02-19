5ad193978e02ed0b9d33116de778fba8e8fd97d7
vanshanand34 <anandvansh34@gmail.com>
2025-02-19 09:45:12 +0530

    Worked in home page and about us page improvements

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import Navbar from "./Navbar";
+import Navbar from "./ClientNavbar";
 import { Metadata } from "next";
 import trackCursor from "./MovingBackground";
+import Link from "next/link";
 
 
 export default function HomePage() {
@@ -16,10 +17,20 @@ export default function HomePage() {
         </div>
 
         <Navbar activeElement="home"></Navbar>
-        <div className="text-3xl md:text-5xl lg:text-6xl text-gray-200 font-bold font-mono mx-[4vw] mt-[10vh]">
+        <div className="text-3xl md:text-6xl lg:text-7xl text-gray-200 font-bold font-mono mx-[4vw] mt-36">
           <div>Backend Developer</div>
           <div>and Tech Enthusiast</div>
         </div>
+
+        <div className="text-2xl text-bold text-gray-300 font-mono mx-[4vw] my-8">
+          I'm Vansh Anand, a Full Stack Developer and coding enthusiast passionate about building scalable applications and crafting efficient solutions. Skilled across front-end and back-end technologies, driven by curiosity and innovation.
+        </div>
+
+        <button className="px-4 py-1 border border-blue-500 shadow-lg mx-[4vw] hover:bg-[#222220]  rounded-lg text-lg">
+          <Link href={"/about-me"}>
+            About Me
+          </Link>
+        </button>
       </div >
     </>
   );
--- app/about-me/AboutPage.tsx
+++ app/about-me/AboutPage.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from "react";
-import Navbar from "../Navbar";
+import Navbar from "../ClientNavbar";
 import Link from "next/link";
 import LinkButtons from "../LinkButtons";
 import trackCursor from "../MovingBackground";
@@ -9,11 +9,13 @@ export default function Home() {
 
   return (
     <>
-      <div className="h-screen w-full " id="container" 
+      <div className="h-screen w-full " id="container"
         onMouseMove={(event: React.MouseEvent) => trackCursor(event)}>
         {/* Navbar */}
         <div id="cursor"
-          className="-z-10 fixed w-[55vw] h-[55vw] bg-[#0099ff0f] rounded-full blur-3xl overflow-hidden"></div>
+          className="-z-10 fixed w-[55vw] h-[55vw] bg-[#0099ff0f] rounded-full blur-3xl overflow-hidden">
+        </div>
+
         <Navbar activeElement="about-me"></Navbar>
 
         {/* <div className="text-center w-full md:text-5xl text-5xl font-semibold mt-[6vh] text-blue-400 underline">About Me</div> */}
@@ -44,7 +46,7 @@ export default function Home() {
           <div className="lg:mx-4 mx-6 mt-6 p-4 lg:ml-[24vw]" id="intro">
             <div className="mx-4 py-4">
 
-              <div className="md:text-7xl text-5xl font-bold py-4">
+              <div className="md:text-7xl text-5xl font-bold my-4">
                 Vansh Anand
               </div>
 
@@ -98,39 +100,41 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="my-12 lg:mx-4 mx-6 p-4 lg:ml-[24vw]" id="experience">
+          <div className="my-12 lg:mx-4 mx-6 p-4 lg:ml-[24vw]" id="education">
             <div className="font-bold md:text-6xl text-4xl my-6 mx-4">
               Education
             </div>
             <div className="mx-5">
-              <div className="flex items-center justify-between">
-                <div className="py-4 font-bold lg:text-3xl text-2xl text-gray-300">
-                  GTBIT (IPU)
+              <div className="my-4">
+                <div className="flex items-center justify-between">
+                  <div className="py-4 font-bold lg:text-3xl text-2xl text-gray-300">
+                    GTBIT (IPU)
+                  </div>
+
+                  <div className="font-semibold text-md text-gray-400">
+                    NOV 2022- AUG 2026
+                  </div>
                 </div>
 
-                <div className="font-semibold text-md text-gray-400">
-                  NOV 2022- AUG 2026
+                <div className="text-lg font-semibold text-blue-400 px-1">
+                  B-TECH (IT)
                 </div>
               </div>
+              <div className="my-4">
+                <div className="flex items-center justify-between">
+                  <div className="py-4 font-bold lg:text-2xl text-xl text-gray-300">
+                    Kendriya Vidyalaya
+                  </div>
 
-              <div className="text-lg font-semibold text-blue-400 px-1">
-                B-TECH (IT)
+                  <div className="font-semibold text-md text-gray-400">
+                    JUNE 2014- AUG 2022
+                  </div>
+                </div>
+
+                <div className="text-lg font-semibold text-blue-400 px-1">
+                  5-12TH
+                </div>
               </div>
-
-              <ul className="marker:text-blue-300 list-disc list-inside py-4 md:text-lg text-md px-2 font-semibold">
-                <li className="py-2">
-                  Developed and optimized Django backend and dashboard performance with efficient ORM queries.
-                </li>
-                <li className="py-2">
-                  Built and integrated RESTful APIs using Django Rest Framework (DRF) for seamless data exchange.
-                </li>
-                <li className="py-2">
-                  Implemented chat and audio call functionality using Agora services in Django.
-                </li>
-                <li className="py-2">
-                  Integrated seller APIs to enhance system capabilities.
-                </li>
-              </ul>
 
             </div>
           </div>
