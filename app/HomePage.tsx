e85683819ef73d6c0b737da272e615b0e78162d3
vanshanand34 <anandvansh34@gmail.com>
2025-02-19 14:17:02 +0530

    Added footer on home page and made link buttons component reusable

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -4,6 +4,7 @@ import Navbar from "./ClientNavbar";
 import { Metadata } from "next";
 import trackCursor from "./MovingBackground";
 import Link from "next/link";
+import LinkButtons from "./LinkButtons";
 
 
 export default function HomePage() {
@@ -17,20 +18,31 @@ export default function HomePage() {
         </div>
 
         <Navbar activeElement="home"></Navbar>
-        <div className="text-3xl md:text-6xl lg:text-7xl text-gray-200 font-bold font-mono mx-[4vw] mt-36">
+        <div className="text-5xl md:text-6xl lg:text-7xl text-gray-200 font-bold font-mono mx-[4vw] mt-36">
           <div>Backend Developer</div>
           <div>and Tech Enthusiast</div>
         </div>
 
-        <div className="text-2xl text-bold text-gray-300 font-mono mx-[4vw] my-8">
+        <div className="md:text-2xl text-lg text-bold text-gray-300 font-mono mx-[4vw] my-8">
           I'm Vansh Anand, a Full Stack Developer and coding enthusiast passionate about building scalable applications and crafting efficient solutions. Skilled across front-end and back-end technologies, driven by curiosity and innovation.
         </div>
 
-        <button className="px-4 py-1 border border-blue-500 shadow-lg mx-[4vw] hover:bg-[#222220]  rounded-lg text-lg">
+        <button className="px-4 py-1 border border-blue-500 shadow-lg mx-[4vw] hover:bg-gray-900  rounded-lg text-lg">
           <Link href={"/about-me"}>
             About Me
           </Link>
         </button>
+
+        <footer className="flex justify-between p-4 items-center mt-24">
+          <div className="text-gray-400 md:px-12">
+            Created By Vansh Anand
+          </div>
+
+          <div>
+            <LinkButtons isHomePage={true} height={12} width={12}/>
+          </div>
+
+        </footer>
       </div >
     </>
   );
--- app/LinkButtons.tsx
+++ app/LinkButtons.tsx
@@ -5,14 +5,18 @@ import GithubLogo from "@/public/GithubLogo";
 import LinkedinLogo from "@/public/LinkedinLogo";
 import GmailLogo from "@/public/GmailLogo";
 
-export default function LinkButtons(): JSX.Element {
+export default function LinkButtons(
+    { isHomePage = false, width = 18, height = 18 }: { isHomePage?: boolean, width?: number, height?: number }
+): JSX.Element {
+
+    const buttonTextStyle: string = isHomePage ? "hidden" : "md:text-lg md:block hidden";
     return (
         <div className="py-4 inline-flex items-center gap-2 flex-wrap">
             <Link href="https://github.com/vanshanand34">
                 <button className="inline-flex items-center space-between hover:bg-[#222220] md:font-semibold 
                 py-2 md:px-4 px-2 border border-blue-300 rounded-lg bg-[#28282838] m-1">
-                    <GithubLogo />
-                    <div className="md:text-lg md:block hidden">
+                    <GithubLogo width={width} height={height}/>
+                    <div className={buttonTextStyle}>
                         Github
                     </div>
                 </button>
@@ -20,8 +24,8 @@ export default function LinkButtons(): JSX.Element {
             <Link href="https://www.linkedin.com/in/anandvansh/">
                 <button className="inline-flex items-center space-between hover:bg-[#222220] md:font-semibold 
                 py-2 md:px-4 px-2 border border-blue-300 rounded-lg bg-[#28282838] m-1">
-                    <LinkedinLogo />
-                    <div className="md:text-lg md:block hidden">
+                    <LinkedinLogo width={width} height={height}/>
+                    <div className={buttonTextStyle}>
                         LinkedIn
                     </div>
                 </button>
@@ -29,8 +33,8 @@ export default function LinkButtons(): JSX.Element {
             <Link href="anandvansh34@gmail.com">
                 <button className="inline-flex items-center space-between hover:bg-[#222220] md:font-semibold 
                 py-2 md:px-4 px-2 border border-blue-300 rounded-lg bg-[#28282838] m-1">
-                    <GmailLogo />
-                    <div className="md:text-lg md:block hidden">
+                    <GmailLogo width={width} height={height}/>
+                    <div className={buttonTextStyle}>
                         Email
                     </div>
                 </button>
@@ -38,3 +42,4 @@ export default function LinkButtons(): JSX.Element {
         </div>
     )
 }
+
