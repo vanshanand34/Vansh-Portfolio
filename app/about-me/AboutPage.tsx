1e33b5cb1a863c05257d3725060689e1b7974111
vanshanand34 <anandvansh34@gmail.com>
2025-02-19 14:16:11 +0530

    fixed spacing and font sized in about us page

--- app/about-me/AboutPage.tsx
+++ app/about-me/AboutPage.tsx
@@ -43,8 +43,8 @@ export default function Home() {
         </div>
 
         <div className="py-4 md:mr-32">
-          <div className="lg:mx-4 mx-6 mt-6 p-4 lg:ml-[24vw]" id="intro">
-            <div className="mx-4 py-4">
+          <div className="lg:mx-4 md:mx-6 mx-4 mt-12 p-4 lg:ml-[24vw]" id="intro">
+            <div className="md:mx-4 py-4">
 
               <div className="md:text-7xl text-5xl font-bold my-4">
                 Vansh Anand
@@ -63,26 +63,26 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="my-12 lg:mx-4 mx-6 p-4 lg:ml-[24vw]" id="experience">
-            <div className="font-bold md:text-6xl text-4xl my-6 mx-4">
+          <div className="my-12 lg:mx-4 md:mx-6 mx-4 p-4 lg:ml-[24vw]" id="experience">
+            <div className="font-bold md:text-6xl text-4xl my-6 md:mx-4">
               Work Experience
             </div>
-            <div className="mx-5">
+            <div className="md:mx-5">
               <div className="flex items-center justify-between">
-                <div className="py-4 font-bold lg:text-3xl text-2xl text-gray-300">
+                <div className="font-bold lg:text-3xl md:text-2xl text-lg text-gray-300">
                   Ayuvya Ayurveda
                 </div>
 
-                <div className="font-semibold text-md text-gray-400">
+                <div className="my-4 md:font-semibold md:text-md text-xs text-gray-400">
                   JULY 2024- JAN 2025
                 </div>
               </div>
 
-              <div className="text-lg font-semibold text-blue-400 px-1">
+              <div className="md:text-lg font-semibold text-blue-400 px-1">
                 Backend Developer Intern
               </div>
 
-              <ul className="marker:text-blue-300 list-disc list-inside py-4 md:text-lg text-md px-2 font-semibold">
+              <ul className="font-sans marker:text-blue-300 list-disc list-inside py-4 md:text-lg text-md px-2 md:font-semibold">
                 <li className="py-2">
                   Developed and optimized Django backend and dashboard performance with efficient ORM queries.
                 </li>
@@ -101,37 +101,37 @@ export default function Home() {
           </div>
 
           <div className="my-12 lg:mx-4 mx-6 p-4 lg:ml-[24vw]" id="education">
-            <div className="font-bold md:text-6xl text-4xl my-6 mx-4">
+            <div className="font-bold md:text-6xl text-4xl my-6 md:mx-4">
               Education
             </div>
-            <div className="mx-5">
+            <div className="md:mx-5">
               <div className="my-4">
                 <div className="flex items-center justify-between">
-                  <div className="py-4 font-bold lg:text-3xl text-2xl text-gray-300">
+                  <div className="py-4 font-bold lg:text-3xl md:text-2xl text-lg text-gray-300">
                     GTBIT (IPU)
                   </div>
 
-                  <div className="font-semibold text-md text-gray-400">
+                  <div className="font-semibold md:text-md text-sm text-gray-400">
                     NOV 2022- AUG 2026
                   </div>
                 </div>
 
-                <div className="text-lg font-semibold text-blue-400 px-1">
+                <div className="md:text-lg text-sm font-semibold text-blue-400 px-1">
                   B-TECH (IT)
                 </div>
               </div>
               <div className="my-4">
                 <div className="flex items-center justify-between">
-                  <div className="py-4 font-bold lg:text-2xl text-xl text-gray-300">
+                  <div className="py-4 font-bold lg:text-2xl md:text-xl text-lg text-gray-300">
                     Kendriya Vidyalaya
                   </div>
 
-                  <div className="font-semibold text-md text-gray-400">
+                  <div className="font-semibold md:text-md text-sm text-gray-400">
                     JUNE 2014- AUG 2022
                   </div>
                 </div>
 
-                <div className="text-lg font-semibold text-blue-400 px-1">
+                <div className="md:text-lg text-sm font-semibold text-blue-400 md:px-1">
                   5-12TH
                 </div>
               </div>
